9d97c816b9c40a3b48de0855b70ee62e108670c4
Oklahomawhore <Oklahomawhore@user.noreply.github.com>
2018-10-11 10:20:29 +0800

    Initial Commit

---
+++ TicketAlert/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TicketAlert
+//
+//  Created by Wangshu Zhu on 2018/10/11.
+//  Copyright © 2018 Wangshu Zhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
